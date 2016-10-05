f8e4720914670195ed4d3ee52a8522c520be2dab
Juan Pedro <juanpe.sn@gmail.com>
2016-10-05 16:03:34 +0200

    cleaning settings and requirements

--- blueliv_exercice/settings/base.py
+++ blueliv_exercice/settings/base.py
@@ -27,11 +27,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
 
     'news_crawler',
-    'kombu.transport.django',
-    'djcelery'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -42,22 +39,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'blueliv_exercice.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
 
 WSGI_APPLICATION = 'blueliv_exercice.wsgi.application'
 
